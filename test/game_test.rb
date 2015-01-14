413b44d9375f2d05f500d4512f76daa815f892ee
Andrew Overby <andrewdoverby@gmail.com>
2015-01-13 19:00:39 -0700

    add row,col, sq work

--- test/game_test.rb
+++ test/game_test.rb
@@ -5,10 +5,28 @@ require 'pry'
 
 class GameTest < Minitest::Test
 
+  def test_game_exists
+    skip
+    #board = Board.new
+    #player = Player.new
+    game = Game.new
+    assert game
+  end
+
+  def test_game_plays_correctly
+    skip
+
+  end
+
+  def test_player_can_win
+    skip
+
+  end
+
+  def test
+    skip
 
 
-  
-
-
+  end
 
 end
--- test/row_test.rb
+++ test/row_test.rb
@@ -16,63 +16,73 @@ class RowTest < Minitest::Test
     assert_equal 9, row.possibilities.length
   end
 
-  def test_row_spots_are_integers
-    input = '123 56789'
-    row = Row.new(input)
-    assert row.input.first.is_a?(Integer)
+  def test_unsolvable_input_returns_original_row
+    row = Row.new('123     9')
+    assert_equal '123000009', row.evaluate
   end
 
-  def test_possibility_in_poss_array_are_integers
-    input = '123 56789'
-    row = Row.new(input)
-    assert row.possibilities.last.is_a?(Integer)
+  def test_row_with_nine_fills_in_empty_spot
+    row = Row.new("1234567 9")
+    assert_equal "8", row.missing_number
+    assert_equal ("123456789"), row.evaluate
   end
 
-  def test_poss_array_exists
-    input = '123 56789'
-    row = Row.new(input)
-    assert row.possibilities
-  end
+  # def test_row_spots_are_integers
+  #   input = '123 56789'
+  #   row = Row.new(input)
+  #   assert row.input.first.is_a?(Integer)
+  # end
+  #
+  # def test_possibility_in_poss_array_are_integers
+  #   input = '123 56789'
+  #   row = Row.new(input)
+  #   assert row.possibilities.last.is_a?(Integer)
+  # end
+  #
+  # def test_poss_array_exists
+  #   input = '123 56789'
+  #   row = Row.new(input)
+  #   assert row.possibilities
+  # end
+  #
+  # def test_it_takes_row_array_as_input
+  #   input = '123 56789'
+  #   row = Row.new(input)
+  #   #input = Array.new
+  #   assert_equal input[8].to_i, row.input.length
+  #   end
+  #
+  # def test_possibilities_array_has_nine_numbers
+  #   input = '123 56789'
+  #   row = Row.new(input)
+  #   assert_equal 9, row.possibilities.length
+  # end
+  #
+  # def test_it_reduces_all_possibilities
+  #   input = '123 56789'
+  #   row = Row.new(input)
+  #   row.reduce_possibility
+  #   assert_equal 1, row.possibilities.length
+  # end
+  #
+  # def test_it_solves_row_missing_one_spot
+  #   input = '123 56789'
+  #   row = Row.new(input)
+  #   assert_equal 9, row.possibilities.length
+  # end
+  #
+  # def test_it_knows_input_array_different_than_poss_array_data
+  #   input = '123 56789'
+  #   row = Row.new(input)
+  #   refute_equal input, row.possibilities
+  # end
+  #
+  # def test_it_inserts_zeros_for_input_missing_spots
+  #   input = '123 56789'
+  #   row = Row.new(input)
+  #   assert row.input.include?(0)
+  # end
 
-  def test_it_takes_row_array_as_input
-    input = '123 56789'
-    row = Row.new(input)
-    #input = Array.new
-    assert_equal input[8].to_i, row.input.length
-    end
 
-  def test_possibilities_array_has_nine_numbers
-    input = '123 56789'
-    row = Row.new(input)
-    assert_equal 9, row.possibilities.length
-  end
 
-  def test_it_reduces_all_possibilities
-    input = '123 56789'
-    row = Row.new(input)
-    row.reduce_possibility
-    assert_equal 1, row.possibilities.length
-  end
-
-  def test_it_solves_row_missing_one_spot
-    input = '123 56789'
-    row = Row.new(input)
-    assert_equal 9, row.possibilities.length
-  end
-
-  def test_it_knows_input_array_different_than_poss_array_data
-    input = '123 56789'
-    row = Row.new(input)
-    refute_equal input, row.possibilities
-  end
-
-  def test_it_inserts_zeros_for_input_missing_spots
-    input = '123 56789'
-    row = Row.new(input)
-    assert row.input.include?(0)
-  end
-
-  def test
-    skip
-  end
 end
