1edf95e38b6d5a5528a5b1952e521004cadaee4b
Andrew Overby <andrewdoverby@gmail.com>
2015-01-14 14:33:35 -0700

    insert some boardbuilder tests

---
+++ test/boardbuilder_test.rb
@@ -0,0 +1,46 @@
+require_relative '../lib/boardbuilder'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+class BoardBuilderTest < Minitest::Test
+
+  def test_it_exists
+    assert boardbuilder
+  end
+
+  def test_it_takes_array_of_arrays
+    skip
+
+  end
+
+  def test_array_is_delimited
+    skip
+
+  end
+
+  def test_blank_spots_are_correct
+    skip
+
+  end
+
+  def test_it_implements_rows
+      skip
+
+  end
+
+  def test_it_implements_columns
+    skip
+
+  end
+
+  def test_it_implements_squares
+    skip
+
+  end
+
+  def test_full_board
+    skip
+
+  end
+end
